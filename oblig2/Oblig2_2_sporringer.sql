d5c963db41fabb6b2274087486d6d94826f5aefe
EmilB04 <emil.berglund@live.no>
2024-02-05 12:01:57 +0100

    Videre arbeid på oblig

--- oblig2/Oblig2_2_sporringer.sql
+++ oblig2/Oblig2_2_sporringer.sql
@@ -3,4 +3,10 @@
 /*Vis fornavn, etternavn og høyden til den høyeste pasienten*/
 /*SELECT fornavn, etternavn, hoyde FROM pasienter_med_provins WHERE hoyde = (SELECT MAX(hoyde) FROM pasienter_med_provins);*/
 /*Vis all informasjon tilknyttet pasienter med følgende id: 1,45,234,379,500*/
-SELECT * FROM pasienter_med_provins WHERE pasient_id = '1' or pasient_id = '45' or pasient_id = '234' or pasient_id = '379' or pasient_id = '500';
+/*SELECT * FROM pasienter_med_provins WHERE pasient_id = '1' or pasient_id = '45' or pasient_id = '234' or pasient_id = '379' or pasient_id = '500';*/
+/*Basert på sted en pasient bor, vis de unike stedene som tilhører provins NS*/
+/*SELECT sted FROM pasienter_med_provins WHERE provins_id = "NS";*/
+/*Skriv en spørring som finner fornavn, etternavn og fødselsdag for de pasientene som er høyere enn 160 cm og tyngre enn 70 kg.*/
+/*SELECT fornavn, etternavn, fodselsdag FROM pasienter_med_provins WHERE hoyde > 160 and vekt > 70;*/
+/*Skriv en spørring som lager en liste med alle pasienter som har en kjent allergi og kommer fra Hamilton. Listen skal inkludere fornavn, etternavn og allergier.*/
+/*-----------*/

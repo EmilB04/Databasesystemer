429c814912486dd636ed328375a26e266bd418a8
EmilB04 <emil.berglund@live.no>
2024-02-26 11:18:18 +0100

    Retting

--- oblig2/2_3/CREATE_innleggelser.sql
+++ oblig2/2_3/CREATE_innleggelser.sql
@@ -5,8 +5,7 @@ CREATE TABLE innleggelser (
     diagnose VARCHAR(25),
     lege_id INT NOT NULL,
     
-    CONSTRAINT pk_innleggelser PRIMARY KEY (pasient_id, lege_id),
+    CONSTRAINT pk_innleggelser PRIMARY KEY (pasient_id, lege_id, innleggelsesdato),
     CONSTRAINT fk_oblig1_2 FOREIGN KEY (pasient_id) REFERENCES oblig_1_2(pasient_id),
     CONSTRAINT fk_leger FOREIGN KEY (lege_id) REFERENCES leger(lege_id)
-    
 );

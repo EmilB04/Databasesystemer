45ad976d6645c7abea09bdfbc913be7304824228
EmilB04 <emil.berglund@live.no>
2024-03-15 14:00:10 +0100

    Endret navn og levert eksamen

--- eksamen_del1/CREATE_ukePlan.sql
+++ eksamen_del1/CREATE_ukePlan.sql
@@ -1,19 +1,7 @@
 CREATE TABLE ukePlan (
     PlanID INT NOT NULL,
-    mandag INT,
-    tirsdag INT, 
-    onsdag INT, 
-    torsdag INT,
-    fredag INT,
-    lordag INT,
-    sondag INT,
+    program_id INT NOT NULL,
     
     CONSTRAINT pk_uPlan PRIMARY KEY (PlanID),
-    CONSTRAINT fk_mandag FOREIGN KEY (mandag) REFERENCES programmer(program_id),
-    CONSTRAINT fk_tirsdag FOREIGN KEY (tirsdag) REFERENCES programmer(program_id),
-    CONSTRAINT fk_onsdag FOREIGN KEY (onsdag) REFERENCES programmer(program_id),
-    CONSTRAINT fk_torsdag FOREIGN KEY (torsdag) REFERENCES programmer(program_id),
-    CONSTRAINT fk_fredag FOREIGN KEY (fredag) REFERENCES programmer(program_id),
-    CONSTRAINT fk_lordag FOREIGN KEY (lordag) REFERENCES programmer(program_id),
-    CONSTRAINT fk_sondag FOREIGN KEY (sondag) REFERENCES programmer(program_id)
+    CONSTRAINT fk_program FOREIGN KEY (program_id) REFERENCES programmer(program_id)
 );
--- eksamen_del1/INSERT_okt.sql
+++ eksamen_del1/INSERT_okt.sql
@@ -1,23 +1,22 @@
--- Legg til nye rader i tabellen okt, med oktID som starter på 1
 INSERT INTO okt (oktID, oktType, brukerID, dato) VALUES
 (1, 'Styrkeøkt', 1, '2024-03-10'),
 (2, 'Kondisjonsøkt', 1, '2024-03-11'),
-(3, 'Aktiv-hviledag', 1, '2024-03-12'),
+(3, 'Kondisjonsøkt', 1, '2024-03-12'),
 (4, 'Styrkeøkt', 1, '2024-03-13'),
 (5, 'Kondisjonsøkt', 1, '2024-03-14'),
-(6, 'Aktiv-hviledag', 1, '2024-03-15'),
+(6, 'Kondisjonsøkt', 1, '2024-03-15'),
 (7, 'Konkurranse', 1, '2024-03-16'),
 (8, 'Kondisjonsøkt', 2, '2024-03-10'),
 (9, 'Styrkeøkt', 2, '2024-03-11'),
-(10, 'Konkurranse', 2, '2024-03-12'), -- Endret fra 'Aktiv-hviledag' til 'Konkurranse'
+(10, 'Konkurranse', 2, '2024-03-12'), 
 (11, 'Kondisjonsøkt', 2, '2024-03-13'),
 (12, 'Styrkeøkt', 2, '2024-03-14'),
-(13, 'Konkurranse', 2, '2024-03-15'), -- Endret fra 'Aktiv-hviledag' til 'Konkurranse'
+(13, 'Konkurranse', 2, '2024-03-15'),
 (14, 'Styrkeøkt', 2, '2024-03-16'),
 (15, 'Styrkeøkt', 3, '2024-03-10'),
 (16, 'Kondisjonsøkt', 3, '2024-03-11'),
-(17, 'Konkurranse', 3, '2024-03-12'), -- Endret fra 'Aktiv-hviledag' til 'Konkurranse'
+(17, 'Konkurranse', 3, '2024-03-12'),
 (18, 'Styrkeøkt', 3, '2024-03-13'),
 (19, 'Kondisjonsøkt', 3, '2024-03-14'),
-(20, 'Konkurranse', 3, '2024-03-15'), -- Endret fra 'Aktiv-hviledag' til 'Konkurranse'
+(20, 'Konkurranse', 3, '2024-03-15'), 
 (21, 'Konkurranse', 3, '2024-03-16');

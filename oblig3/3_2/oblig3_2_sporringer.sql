16330d65473e4a9a17b02589a0a6e89707983d43
EmilB04 <emil.berglund@live.no>
2024-04-02 12:24:56 +0200

    fullført spørringer oblig 3.2

--- oblig3/3_2/oblig3_2_sporringer.sql
+++ oblig3/3_2/oblig3_2_sporringer.sql
@@ -38,3 +38,34 @@ JOIN innleggelser i ON l.lege_id = i.lege_id
 GROUP BY l.lege_id, fullt_navn;
 
 -- Vis totalt antall pasienter for hver provins (ved navn). Sorter fra høyest til lavest antall -- 
+SELECT pmp.provins_id, p.provins_navn, COUNT(*) as totalt_antall_pasienter
+FROM pasienter_med_provins as pmp
+JOIN provins p ON pmp.provins_id = p.provins_id
+GROUP BY pmp.provins_id, p.provins_navn
+ORDER BY totalt_antall_pasienter DESC;
+
+-- For hver innleggelse, vis en pasients fulle navn, deres diagnose og det fulle navnet til legen som satt diagnosen -- 
+SELECT CONCAT(pmp.fornavn, ' ', pmp.etternavn) AS pasient_navn, i.diagnose, CONCAT(l.fornavn, ' ', l.etternavn) AS lege_navn
+FROM innleggelser i
+JOIN leger l ON i.lege_id = l.lege_id
+JOIN pasienter_med_provins pmp ON i.pasient_id = pmp.pasient_id;
+
+-- Vis antall duplikate pasienter basert på fornavn og etternavn. Vis dette kun for de der det finnes duplikater -- 
+SELECT fornavn, etternavn, COUNT(*) as duplikater
+FROM pasienter_med_provins
+GROUP BY fornavn, etternavn
+HAVING COUNT(*) > 1;
+
+-- Vis pasientenes fulle navn, høyde i fot avrundet til ett tall etter komma, vekt i pund avrundet til ingen desimaler, fødselsnummer og kjønn(ikke forkortet).  -- 
+SELECT CONCAT(pmp.fornavn, ' ', pmp.etternavn) AS navn,
+       ROUND(hoyde/30.48, 1) AS hoyde_i_fot,
+       ROUND(vekt * 2.205) AS vekt_i_pund,
+       fodselsdag,
+       IF(kjonn = 'M', 'Mann', 'Dame') AS kjonn
+FROM pasienter_med_provins pmp;
+
+-- Vis pasient ID, fornavn og etternavn for pasienter som aldri har vært innlagt. -- 
+SELECT pasient_id, fornavn, etternavn
+FROM pasienter_med_provins
+WHERE pasient_id NOT IN 
+	(SELECT pasient_id FROM innleggelser);
